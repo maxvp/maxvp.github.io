4eb18c0f19635625003aae5a398916787d7ee0cd
Max <maxvphillips@gmail.com>
2022-09-11 23:03:50 -0400

    Prevent date repeat

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ var options = {
     valueNames: [
         "title",
         { name: "date", attr: "real-date" },
-        "type",
+        "client",
         "description",
     ],
 };
 
 var archiveList = new List("archive", options);
+
+// Prevent date repeat
+var dateList = document.getElementsByClassName("date");
+var previousDate = 0;
+
+for (let i = 0; i < archiveList.items.length; i++) {
+    let currentDiv = dateList[i];
+    var currentDate = currentDiv.getAttribute("real-date");
+    if (currentDate == previousDate) {
+        document.getElementsByClassName("date")[i].style.display = "none";
+    }
+    previousDate = currentDate;
+}
