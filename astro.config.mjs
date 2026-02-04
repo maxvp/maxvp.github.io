4e4bb69ee39c98c8a0aafd0e94cc6ab57f4835c3
Max <maxvphillips@gmail.com>
2026-02-04 13:52:00 -0600

    remove redundant packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxvphillips.com',
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'auto',
   },
+
+  integrations: [mdx(), sitemap()],
 });
